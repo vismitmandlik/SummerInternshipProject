09e8d61487ef4dea94883ca7402514e271e75f62
Dhruvi Ladani <110341143+dhruviladani@users.noreply.github.com>
2024-03-13 18:54:24 +0530

    Update change-password.js

--- src/routes/change-password.js
+++ src/routes/change-password.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { User } = require("../models/product");
+const { User } = require("../models/product.model");
 
 router.get("/", async (req, res) => {
     try {
