87c3a27deb6a45373783b5fc2ea7fead75a956c6
vismitmandlik <vismitmandlik03@gmail.com>
2024-03-13 21:20:20 +0530

    refactoring

--- src/modules/auth/auth.route.js
+++ src/modules/auth/auth.route.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const { ProductModel, UserModel } = require('../models');
 
-// Home Route
 router.get('/', async (req, res) => {
   try {
     res.render('login');
